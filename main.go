0f602445cd4dd74281e427d667fe64df2626a8d2
Júlio Agripino <juliocesar014@live.com>
2023-09-20 13:57:18 -0300

    feat: Add crud with token response in login router

--- main.go
+++ main.go
@@ -7,32 +7,32 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func main() {
-	//connect to database
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	//create the table if it doesn't exist
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT, password TEXT)")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//create router
 	router := mux.NewRouter()
 	router.HandleFunc("/users", getUsers(db)).Methods("GET")
 	router.HandleFunc("/users/{id}", getUser(db)).Methods("GET")
@@ -40,8 +40,8 @@ func main() {
 	router.HandleFunc("/users/{id}", updateUser(db)).Methods("PUT")
 	router.HandleFunc("/users/{id}", deleteUser(db)).Methods("DELETE")
 	router.HandleFunc("/health", healthCheck()).Methods("GET")
+	router.HandleFunc("/login", login(db, []byte("secret-key"))).Methods("POST")
 
-	//start server
 	log.Fatal(http.ListenAndServe(":8000", jsonContentTypeMiddleware(router)))
 }
 
@@ -52,10 +52,9 @@ func jsonContentTypeMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-// get all users
 func getUsers(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT * FROM users")
+		rows, err := db.Query("SELECT id, name, email, password FROM users")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -64,7 +63,7 @@ func getUsers(db *sql.DB) http.HandlerFunc {
 		users := []User{}
 		for rows.Next() {
 			var u User
-			if err := rows.Scan(&u.ID, &u.Name, &u.Email); err != nil {
+			if err := rows.Scan(&u.ID, &u.Name, &u.Email, &u.Password); err != nil {
 				log.Fatal(err)
 			}
 			users = append(users, u)
@@ -77,14 +76,13 @@ func getUsers(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-// get user by id
 func getUser(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id := vars["id"]
 
 		var u User
-		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&u.ID, &u.Name, &u.Email)
+		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&u.ID, &u.Name, &u.Email, &u.Password)
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
 			return
@@ -94,22 +92,32 @@ func getUser(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-// create user
 func createUser(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var u User
 		json.NewDecoder(r.Body).Decode(&u)
 
-		err := db.QueryRow("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id", u.Name, u.Email).Scan(&u.ID)
+		if u.Name == "" || u.Email == "" || u.Password == "" {
+			http.Error(w, "Name, email, and password are required", http.StatusBadRequest)
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
 		if err != nil {
-			log.Fatal(err)
+			http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+			return
+		}
+
+		err = db.QueryRow("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id", u.Name, u.Email, string(hashedPassword)).Scan(&u.ID)
+		if err != nil {
+			http.Error(w, "Failed to create user", http.StatusInternalServerError)
+			return
 		}
 
 		json.NewEncoder(w).Encode(u)
 	}
 }
 
-// update user
 func updateUser(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var u User
@@ -118,7 +126,7 @@ func updateUser(db *sql.DB) http.HandlerFunc {
 		vars := mux.Vars(r)
 		id := vars["id"]
 
-		_, err := db.Exec("UPDATE users SET name = $1, email = $2 WHERE id = $3", u.Name, u.Email, id)
+		_, err := db.Exec("UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4", u.Name, u.Email, u.Password, id)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -127,30 +135,37 @@ func updateUser(db *sql.DB) http.HandlerFunc {
 	}
 }
 
-// delete user
 func deleteUser(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id := vars["id"]
 
+		if id == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]string{"message": "Invalid ID"})
+			return
+		}
+
 		var u User
-		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&u.ID, &u.Name, &u.Email)
+		err := db.QueryRow("SELECT * FROM users WHERE id = $1", id).Scan(&u.ID, &u.Name, &u.Email, &u.Password)
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]string{"message": "User not found"})
 			return
-		} else {
-			_, err := db.Exec("DELETE FROM users WHERE id = $1", id)
-			if err != nil {
-				w.WriteHeader(http.StatusNotFound)
-				return
-			}
-
-			json.NewEncoder(w).Encode("User deleted")
 		}
+
+		_, err = db.Exec("DELETE FROM users WHERE id = $1", id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]string{"message": "Failed to delete user"})
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"message": "User deleted"})
 	}
 }
 
-// health check
 func healthCheck() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		info := map[string]string{
@@ -167,5 +182,53 @@ func healthCheck() http.HandlerFunc {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(jsonData)
+	}
+}
+
+func login(db *sql.DB, secretKey []byte) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var loginData struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&loginData)
+		if err != nil {
+			http.Error(w, "Invalid login data", http.StatusBadRequest)
+			return
+		}
+
+		// Verifique o email do usuário
+		var storedUser User
+		err = db.QueryRow("SELECT * FROM users WHERE email = $1", loginData.Email).Scan(&storedUser.ID, &storedUser.Name, &storedUser.Email, &storedUser.Password)
+		if err != nil {
+			http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+			return
+		}
+
+		// Verifique a senha
+		err = bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(loginData.Password))
+		if err != nil {
+			http.Error(w, "Invalid email or password", http.StatusUnauthorized)
+			return
+		}
+
+		// Gere o token JWT
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"userId": storedUser.ID,
+		})
+
+		tokenString, err := token.SignedString(secretKey)
+		if err != nil {
+			http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+			return
+		}
+
+		// Retorne o token gerado como resposta
+		response := map[string]string{
+			"token": tokenString,
+		}
+
+		json.NewEncoder(w).Encode(response)
 	}
 }
